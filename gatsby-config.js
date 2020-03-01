fc4360fa65e815ecd5b6d5d19f2a266d620a51fc
shekharmayank <mayankshekhar03@live.in>
2020-03-01 11:53:46 +0530

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,28 +46,28 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: process.env.UA-82633250-2,
       },
     },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Mayank Shekhar`,
+        short_name: `Mayank`,
+        description: `Web Presence`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
+            src: `/mayank.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/android-chrome-512x512.png`,
+            src: `/mayank.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
